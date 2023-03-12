30cf09f0c8f19ce61fad35ada59a85ecfbb6ea80
Arianit Sadriu <arianitsa1@gmail.com>
2023-03-12 22:18:30 +0100

    commit 3

---
+++ public/includes/menu.php
@@ -0,0 +1,19 @@
+<img src="/public/logo.png" alt="Logo">
+                    <nav>
+                        <ul>
+                            <li><a class="nav-link" href="Homepage.php">Ballina</a></li>
+                            <li><a href="#">Produktet</a>
+                                <div class="sub-menu">
+                                    <ul>
+                                        <li><a class="nav-link" href="/src/pages/Kitchen.php">Kuzhina</a></li>
+                                        <li><a class="nav-link" href="/src/pages/LivingRoom.php">Salloni</a></li>
+                                        <li><a class="nav-link" href="/src/pages/Bedroom.php">Dhoma</a></li>
+                                        <li><a class="nav-link" href="/src/pages/Office.php">Zyre</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a class="nav-link" href="/src/pages/Aboutus.php">Rreth Nesh</a></li>
+                            <li><a class="nav-link" href="/src/pages/Contact.php">Kontakti</a></li>
+                            <li><a class="nav-link" href="">Log-In</a></li>
+                        </ul>
+                    </nav>
